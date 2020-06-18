f397293c0fea398a675c2fbe4b4147c7f9fde781
Marissa Ross <mlyall.ross@ufl.edu>
2020-06-18 16:31:29 -0700

    Add weather description

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ xmlhttp.onreadystatechange = function() {
         var cityName = document.getElementById('header-span');
         cityName.innerHTML = apiResult.name;
 
+        var weatherToday = document.getElementById('main-span');
+        weatherToday.innerHTML = apiResult.main.temp;
     }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=85225,us&appid=6efff70fe1477748e31c17d1c504635f', true);
